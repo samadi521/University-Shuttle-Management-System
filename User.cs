36dfe1961b0696f5843ad702300015e275c723d3
Nethmi Maleesha <pmnmwimalasiri@students.nsbm.ac.lk>
2026-03-23 15:34:25 +0530

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,12 @@
+﻿namespace ShuttleTrackerApp.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? phone { get; set; }
+        public string? password { get; set; }
+        public string? role { get; set; }
+    }
+}
