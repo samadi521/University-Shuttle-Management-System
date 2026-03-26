7989cbd17cd29a2e0b613db8eda8e93bc8382300
Nethmi Maleesha <pmnmwimalasiri@students.nsbm.ac.lk>
2026-03-27 02:43:35 +0530

    Add files via upload

---
+++ LoginResponse.cs
@@ -0,0 +1,7 @@
+﻿public class LoginResponse
+{
+    public string token { get; set; }
+    public string role { get; set; }
+    public int userId { get; set; }
+    public string error { get; set; }
+}
