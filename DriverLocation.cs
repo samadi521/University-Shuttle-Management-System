37d39a6b892d127c8600d70a7717606cb44250a9
samadi521 <sammaniesamaraweera@gmail.com>
2026-03-27 00:08:42 +0530

    Delete DriverLocation.cs

--- DriverLocation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShuttleTrackerApp.Models
-{
-    public class DriverLocation
-    {
-        public int driver_id { get; set; }
-        public double latitude { get; set; }
-        public double longitude { get; set; }
-    }
-}
