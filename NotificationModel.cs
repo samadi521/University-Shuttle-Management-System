c3d76529fbeeaff4edd3e3dfb96e860c65db1248
samadi521 <sammaniesamaraweera@gmail.com>
2026-03-27 00:12:21 +0530

    Delete NotificationModel.cs

--- NotificationModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShuttleTrackerApp.Models
-{
-    public class NotificationModel
-    {
-        public string? message { get; set; }
-        public string? status { get; set; }
-    }
-}
