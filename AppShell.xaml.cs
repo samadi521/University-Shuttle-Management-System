ec1100436d8005e6a6cd0a2923b025515627a7e6
samadi521 <sammaniesamaraweera@gmail.com>
2026-03-27 00:27:43 +0530

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+namespace ShuttleTrackerApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+namespace ShuttleTrackerApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .UseMauiMaps() 
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        return builder.Build();
+    }
+}
