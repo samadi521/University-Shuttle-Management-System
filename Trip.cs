6e97d264b10fb661d904c27838cbcc3b8151af52
Nethmi Maleesha <pmnmwimalasiri@students.nsbm.ac.lk>
2026-03-27 02:46:56 +0530

    Add files via upload

---
+++ Trip.cs
@@ -0,0 +1,8 @@
+﻿public class Trip
+{
+    public int id { get; set; }
+    public string start_location { get; set; }
+    public string end_location { get; set; }
+    public double price { get; set; }
+    public int available_seats { get; set; }
+}
